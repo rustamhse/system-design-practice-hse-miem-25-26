0a365ddf818479156bace579e46a0a587d348fb3
Rustam Zieev <rustam.solidarm@gmail.com>
2025-12-24 17:37:12 +0300

    done hw1

---
+++ demo-app-1/k6/scripts/load-script-storm.js
@@ -0,0 +1,25 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '60s', target: 10000 }
+  ]
+};
+
+function randomInt(min, max){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export default function() {
+  if (Math.random() < 0.8) {
+    const payload = JSON.stringify({ user_id: randomInt(1,2), amount: Math.random()*100, description: 'k6 load' });
+    const res = http.post('http://localhost:8080/api/orders', payload, { headers: { 'Content-Type': 'application/json' } }); // тут поменял порт на 8088, чтобы весь трафик шел через nginx, так корректнее
+    check(res, { 'created': (r) => r.status === 200 || r.status === 201 });
+  } else {
+    const res = http.get('http://localhost:8080/api/orders');
+    check(res, { 'list ok': (r) => r.status === 200 });
+  }
+  sleep(0.05);
+}
+
